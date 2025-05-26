c5741b09eafc3ed4e624ba6dfb87f94a0c04273c
Sadrinho27 <Titouan.tabere@gmail.com>
2025-05-26 19:15:14 +0200

    added chunks for deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base : "/portfolio-tdt2",
+  base: "/portfolio-tdt2",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Évite les warnings de chunk > 500kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          framer: ['framer-motion'],
+          typewriter: ['typewriter-effect'],
+        }
+      }
+    }
+  }
 });
